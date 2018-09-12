5cf6cad2b663e3d60ff5fd3d98255771f7367ded
namtp12 <ntp28459hteam@gmail.com>
2018-09-12 08:04:49 +0700

    Mod 1st time

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,14 +3,20 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
-    // TODO: khai báo các phương thức getter, setter cho Student
-
+    private String name;
+    private String id;
+    private String group;
+    private String emai;
+    
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        name = "Student";
+        id = "000";
+        group = "INT22041";
+        emai = "uet@vnu.edu.vn";
     }
 
     /**
@@ -21,6 +27,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        name = n;
+        id = sid;
+        emai = em;
+        group = "INT22041";
     }
 
     /**
@@ -29,10 +39,95 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        name = s.name;
+        id = s.id;
+        group = s.group;
+        emai = s.emai;
     }
 
-    String getInfo() {
+    // TODO: khai báo các phương thức getter, setter cho Student
+
+    /**
+    * getName
+    * @return 
+    */
+
+    public String getName() {
+    	return name;
+    }
+    
+    /**
+     * setName
+     * @param name
+     */
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * getID
+     * @return
+     */
+    
+    public String getID() {
+        return id;
+    }
+    
+    /**
+     * setID
+     * @param ID
+     */
+    
+    public void setID(String ID) {
+        this.id = ID;
+    }
+    
+    /**
+     * getGroup
+     * @return
+     */
+    
+    public String getGroup() {
+        return group;
+    }
+    
+    /**
+     * setGroup
+     * @param group
+     */
+    
+    public void setGroup(String group) {
+        this.group = group;
+    }
+    
+    /**
+     * getEmai
+     * @return
+     */
+    
+    public String getEmai() {
+        return emai;
+    }
+    
+    /**
+     * setEmai
+     * @param emai
+     */
+    
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    /**
+     * getInfo
+     */
+    public void getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        System.out.println(this.name);
+        System.out.println(this.id);
+        System.out.println(this.group);
+        System.out.println(this.emai);
+        //return null; // xóa dòng này sau khi cài đặt
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,23 +1,67 @@
 package week1;
 
 public class StudentManagement {
-
+    //11
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    static int MAX = 100;
+    Student[] students = new Student[MAX];
 
-    public boolean sameGroup(Student s1, Student s2) {
+    public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        return (s1.getGroup().equals(s2.getGroup()));
+        //return false; // xóa dòng này sau khi cài đặt
     }
-
+    
     void studentsByGroup() {
-        // TODO:
+        //12
+        // TODO: List all the group
+        String[] groups = new String[MAX];
+        int current = 1;
+        groups[0] = students[0].getGroup();
+        for(int i = 0; i < MAX; i++) {
+            
+                
+            }
+        }
     }
 
     void removeStudent(String id) {
-        // TODO:
+        //13
+        // TODO: delete students whose id is equal to id param from list
+        for (int i = 0; i < MAX; i++) {
+            if (students[i].getID().equals(id)) {
+                for (int j = i; j < MAX; j++) {
+                    students[j] = students[j - 1];
+                    MAX--;
+                }
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
         // TODO:
+        Student nam = new Student();
+        nam.setName("Tran Phuong Nam");
+        nam.setID("120");
+        nam.setGroup("INT22044");
+        nam.setEmai("ntp28459hteam");
+        //Print name of the student
+        System.out.println(nam.getName());
+        //Print all info of the student
+        nam.getInfo();
+        //Check 3 construcstors
+        Student s1 = new Student();
+        Student s2 = new Student("S2Name", "S2ID", "S2Emai");
+        Student s3 = new Student(s1);
+        
+        //10: verify mathod sameGroup(Student s1, Student s2)
+        Student s4 = new Student();
+        Student s5 = new Student();
+        //default group is "INT22041"
+        Student s6 = new Student();
+        s6.setGroup("INT22042");
+        System.out.println(sameGroup(s1, s2));
+        System.out.println(sameGroup(s1, s3));
     }
 }
