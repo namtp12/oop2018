f808254271afac2fb8f056b989274b75a0d8df36
namtp12 <ntp28459hteam@gmail.com>
2018-09-19 16:34:08 +0700

    Complete task1, 2a, 2b

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,35 @@
 package week2.task1;
 
+import java.util.Scanner;
+import java.io.PrintStream;
+
 public class Task1 {
 
     public static int gcd(int a, int b) {
-        // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        // TODO 1A: Tính ước chung lớn nhất của 2 số a, b
+		PrintStream print = System.out;
+		Scanner input = new Scanner(System.in);
+		a = input.nextInt();
+		b = input.nextInt();
+		
+		while (a != b) {
+			if (a > b) {
+				a = a - b;
+			}
+			else { //a < b
+				b = b - a;
+			}
+		}
+		
+        return a;
     }
 
     public static int fibonacci(int n) {
-        // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        // TODO 1B: Tìm số fibonacci ở vị trí n
+		if (n == 1 || n == 2) {
+			return 1;
+		}
+		else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -2,29 +2,49 @@ package week2.task2;
 
 public class Fraction {
 
-    // TODO: khai báo các thuộc tính
+    // TODO 2: khai báo các thuộc tính
+	private int numerator;
+	private int denominator;
 
     public Fraction(int numerator, int denominator) {
-        // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
+        // TODO 2A: khởi tạo giá trị cho các thuộc tính numerator (tử số), denominator (mẫu số)
+		this.numerator = numerator;
+		this.denominator = denominator;
     }
 
     public Fraction add(Fraction other) {
-        // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        // TODO 2B: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
+		Fraction result = new Fraction();
+		result.denominator = this.denominator * other.denominator;
+		result.numerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        return result;
     }
 
     public Fraction subtract(Fraction other) {
-        // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        // TODO 2B: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
+        Fraction result = new Fraction();
+		result.denominator = this.denominator * other.denominator;
+		result.numerator = this.numerator * other.denominator - other.numerator * this.denominator;
+        return result;
     }
 
     public Fraction multiply(Fraction other) {
-        // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        // TODO 2B: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
+        Fraction result = new Fraction();
+		result.denominator = this.denominator * other.denominator;
+		result.numerator = this.numerator * other.numerator;
+        return result;
     }
 
     public Fraction divide(Fraction other) {
-        // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        // TODO 2B: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
+        Fraction result = new Fraction();
+		result.numerator = this.numerator * other.denominator;
+		result.denominator = this.denominator * other.numerator;		
+        return result;
     }
+	
+	public boolean equals(Object obj) {
+		//TODO 2C: Phương thức so sánh hai phân số (this và obj), trả về giá trị true hoặc false
+		
 }
