9c58494c53a82c60936e90c09627dd059b60fa84
namtp12 <ntp28459hteam@gmail.com>
2018-09-24 21:47:28 +0700

    Constructing class Cat

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -4,3 +4,33 @@ public class Task3 {
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
+
+class Cat {
+    private String name;
+    private String furColor; //yellow, brown, black, ...
+    private String gender;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String setName(String name) {
+        this.name = name;
+    }
+}
+
+/*
+class smartPhone {
+    private String branch; //Bphone, Xiaomi, ...
+    private int price; //VND
+    private int PIN; //Imei
+    private int simNumber;
+    config c = new config();
+}
+
+class config {
+    private int storageVolume; //GB
+    private float CPUSpeed; //GHz
+    private int height; //mm
+    private int width; //mm
+}*/
