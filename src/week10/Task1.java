517297a5db0100b2f58b1d7c801de0e523561436
namtp12 <ntp28459hteam@gmail.com>
2018-11-10 10:24:53 +0700

    Update #1: Read the file Utils.java and removed tabs TODO: remove the lines contain comment 	  find the common properties of methods name to handle

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,61 @@
 package week10;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+    
+    public static List<String> getAllFunctions(String filePath) {
+        List<String> res = new ArrayList<>();
+        
+        List<String> toPerform = Task1.getStringFromFile(filePath);
+        List<String> performed = Task1.performString(toPerform);
+        for(String s : performed) {
+            System.out.println(s);
+        }
+        
+        return res;
+    }
+    
+    private static ArrayList<String> getStringFromFile(String path) {
+        ArrayList<String> res = null;
+        String line = null; // doesn't cause NullPointerException, although Strings are immutable
+        try {
+            File f = new File(path);
+            FileReader fd = new FileReader(f);
+            BufferedReader buf = new BufferedReader(fd);
+            
+            res = new ArrayList<>();
+            while((line = buf.readLine()) != null) {
+                res.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+    
+    private static List<String> performString (List<String> toPerform) {
+        List<String> performed = new ArrayList<>();
+        
+        for(String s : toPerform) {
+            String removedTabs = s.trim();
+            // Check if current line is a comment
+            if(removedTabs.startsWith("/")) {
+                continue;
+            }
+            if(removedTabs.contains("//")) {
+                //removedTabs.replace("", removedTabs.sub)
+            }
+        }
+        
+        return performed;
+    }
 }
---
+++ src/week10/TestTask1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week10;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TestTask1 {
+    //String path = "\\src\\week9\\Utils.java";
+    static String path = null;
+    
+    public static void main() {
+        path = "src\\week9\\Utils.java";
+        Task1.getAllFunctions(path);
+    }
+}
